61ebf2e19d27b2bf2653c9483e0aef8e6a2d355a
pankaj3649 <pankajrawat3649@gmail.com>
2025-01-17 16:51:43 +0530

    Link ref routing & programatically routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home(){
-    return <div>Home...</div>
+    return <div>
+        <nav>
+            <ul>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/blog">Blog</Link></li>
+                <li><Link href="/docs">Docs</Link></li>
+            </ul>
+        </nav>
+        Home...
+
+    </div>
 }
